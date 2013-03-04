39181dcf064265a535f73b9796d2bb453617f45c
ivar <ivar@ghostly.(none)>
2013-03-04 18:39:23 +0100

    Beginning work on OCL codegen. Initial cleanup.

--- GenOCL.hs
+++ GenOCL.hs
@@ -11,14 +11,37 @@ import Data.Maybe
 import Control.Monad.State
 
 
- 
-{- 
- - My idea: Generate regular C, but offload Parallel loops to GPU via OpenCL interface
--}
-
--- TODO Text.PrettyPrint
-
 gen :: Program a -> Gen ()
+
+gen Skip = line ""
+
+gen (Assign name es e) = line $ show (Index name es) 
+                      ++ " = " ++ show e ++ ";"
+
+gen (p1 :>> p2) = gen p1 >> gen p2
+
+gen (If c p1 p2) = do line $ "if( " ++ show c ++ " ) { "
+                      indent 2
+                      gen p1
+                      unindent 2
+                      line "else { "
+                      indent 2
+                      gen p2
+                      unindent 2
+                      line "}"
+
+gen Par{} = line "// Par in host code"
+
+
+gen (For e1 e2 p) = do i <- fmap fst newLoopVar
+                       line $ show TInt ++ " " ++ i ++ ";"
+                       line $ "for( " ++ i ++ " = " ++ show e1 ++ "; " 
+                           ++ i ++ " < " ++ show e2 ++ "; "
+                           ++ i ++ "++ ) {"
+                       indent 2
+                       gen $ p (var i)
+                       unindent 2
+                       line "}"
 
 gen (Alloc t dim f) = do d <- incVar
                          let m = "mem" ++ show d
@@ -28,39 +51,6 @@ gen (Alloc t dim f) = do d <- incVar
                          gen $ f (locNest m) (Index m)
 
                          line $ "free(" ++ m ++ ");\n"
-
-
-gen Skip = line ""
-
-gen (Assign name es e) = line $ show (Index name es) ++ " = " ++ show e ++ ";"
-
-gen (p1 :>> p2) = gen p1 >> gen p2
-
-gen (If c p1 p2) = do
-  line $ "if( " ++ show c ++ " ) { "
-  indent 2
-  gen p1
-  unindent 2
-  line "else { "
-  indent 2
-  gen p2
-  unindent 2
-  line "}"
-
-gen Par{} = line "// Par in host code"
-
-
-gen (For e1 e2 p) = do
-   d <- incVar
-   let i = ([ "i", "j", "k" ] ++ [ "i" ++ show i | i <- [0..] ]) !! d
-   line $ show TInt ++ " " ++ i ++ ";"
-   line $ "for( " ++ i ++ " = " ++ show e1 ++ "; " 
-                  ++ i ++ " < " ++ show e2 ++ "; " ++ i ++ "++ ) {"
-   indent 2
-   gen $ p (var i)
-   unindent 2
-   line "}"
-
 ---gen (Alloc siz f) = do 
 --   d <- incVar
 --   let m = "mem" ++ show d
@@ -229,57 +219,57 @@ gen (For e1 e2 p) = do
 -- Extras
 
 setupHeadings :: Gen ()
-setupHeadings = do
-  line "#include <stdio.h>"
-  line "#include <stdlib.h>"
-  line "#include <CL/cl.h>"
-  line "#define MAX_SOURCE_SIZE (0x100000)\n\n"
-  line "int main (void) {"
-  indent 2
+setupHeadings = do line "#include <stdio.h>"
+                   line "#include <stdlib.h>"
+                   line "#include <CL/cl.h>"
+                   line "#define MAX_SOURCE_SIZE (0x100000)\n\n"
+                   line "int main (void) {"
+                   indent 2
 
 setupEnd :: Gen ()
 setupEnd = line "return 0;" >> unindent 2 >> line "}"
 
 setupOCL :: Gen ()
-setupOCL = do
-  let fp     = "fp"
-      srcStr = "source_str"
-      srcSize = "source_size"
-  kernels <- getKernelFile
+setupOCL = do let fp     = "fp"
+                  srcStr = "source_str"
+                  srcSize = "source_size"
+              kernels <- getKernelFile
 
-  line $ "FILE *" ++ fp ++ " = NULL;"
-  line $ "char* " ++ srcStr ++ ";"
-  line $ fp ++ " = fopen( \"" ++ kernels ++ "\" , \"r\");"
-  line $ srcStr ++ " = (char*) malloc(MAX_SOURCE_SIZE);"
-  line $ "size_t " ++ srcSize ++ " = fread( " ++ srcStr ++ ", " ++ "1, " ++
-                    "MAX_SOURCE_SIZE, " ++ fp ++ ");"
-  line $ "fclose( " ++ fp ++ " );"
-  
-  let platformID   = "platform_id"
-      deviceID     = "device_id"
-      numDevices   = "ret_num_devices"
-      numPlatforms = "ret_num_platforms"
-      context      = "context"
-      queue        = "command_queue"
-      
-  line $ "cl_platform_id " ++ platformID ++ " = NULL;"
-  line $ "cl_device_id " ++ deviceID ++ " = NULL;"
-  line $ "cl_uint " ++ numDevices ++ ";"
-  line $ "cl_uint " ++ numPlatforms ++ ";"
-  line $ "clGetPlatformIDs(1, &" ++ platformID ++ ", &" ++ numPlatforms ++ ");"
-  line $ "clGetDeviceIDs(" ++ platformID ++ ", CL_DEVICE_TYPE_DEFAULT, 1, " ++
-         "&" ++ deviceID ++ ", &" ++ numDevices ++ ");"
-  line $ "cl_context " ++ context ++ " = clCreateContext(NULL, 1, &" ++ deviceID ++ ", NULL, NULL, NULL);"
-  line $ "cl_command_queue " ++ queue ++ " = clCreateCommandQueue(" ++ context ++ 
-         ", " ++ deviceID ++ ", 0, NULL);"
-  line "\n\n"
+              line $ "FILE *" ++ fp ++ " = NULL;"
+              line $ "char* " ++ srcStr ++ ";"
+              line $ fp ++ " = fopen( \"" ++ kernels ++ "\" , \"r\");"
+              line $ srcStr ++ " = (char*) malloc(MAX_SOURCE_SIZE);"
+              line $ "size_t " ++ srcSize ++ " = fread( " ++ srcStr ++ ", " ++ "1, " ++
+                                "MAX_SOURCE_SIZE, " ++ fp ++ ");"
+              line $ "fclose( " ++ fp ++ " );"
+              
+              let platformID   = "platform_id"
+                  deviceID     = "device_id"
+                  numDevices   = "ret_num_devices"
+                  numPlatforms = "ret_num_platforms"
+                  context      = "context"
+                  queue        = "command_queue"
+                  
+              line $ "cl_platform_id " ++ platformID ++ " = NULL;"
+              line $ "cl_device_id " ++ deviceID ++ " = NULL;"
+              line $ "cl_uint " ++ numDevices ++ ";"
+              line $ "cl_uint " ++ numPlatforms ++ ";"
+              line $ "clGetPlatformIDs(1, &" ++ platformID ++ ", &" ++ numPlatforms ++ ");"
+              line $ "clGetDeviceIDs(" ++ platformID ++ ", CL_DEVICE_TYPE_DEFAULT, 1, " ++
+                     "&" ++ deviceID ++ ", &" ++ numDevices ++ ");"
+              line $ "cl_context " ++ context ++ " = clCreateContext(NULL, 1, &" ++ deviceID ++ ", NULL, NULL, NULL);"
+              line $ "cl_command_queue " ++ queue ++ " = clCreateCommandQueue(" ++ context ++ 
+                     ", " ++ deviceID ++ ", 0, NULL);"
+              line "\n\n"
 
 
+-- TODO FIXME I'm broken and outdated. 
 setupPrint :: String -> Int -> Gen ()
-setupPrint alloc len = do
-  d <- incVar
-  let loopVar =  ([ "i", "j", "k" ] ++ [ "i" ++ show i | i <- [0..] ]) !! d
-  line $ "int " ++ loopVar ++ ";"
-  line $ "for (" ++ loopVar ++ " = 0; " ++ loopVar ++ " < " ++ show len ++ "; " ++ loopVar ++ "++ ) {"
-  line $ "printf(\"%d\\n\"," ++ alloc ++ "[" ++ loopVar ++ "] );"
-  line "}"
+setupPrint alloc len = do loopVar <- fmap fst newLoopVar
+                          line $ "int " ++ loopVar ++ ";"
+                          line $ "for (" ++ loopVar ++ " = 0; " 
+                              ++ loopVar ++ " < " ++ show len ++ "; "
+                              ++ loopVar ++ "++ ) {"
+                          line $ "printf(\"%d\\n\"," ++ alloc 
+                              ++ "[" ++ loopVar ++ "] );"
+                          line "}"
