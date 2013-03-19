08655b1833fb7fa9fd860d799d67ccdf221eb8da
ivar <ivar@8560w.(none)>
2013-03-19 12:11:06 +0100

    exprAsParam doesn't care about scalar names.

--- Analysis.hs
+++ Analysis.hs
@@ -9,6 +9,7 @@ import Program
 import Types
 
 import Data.List
+import Control.Monad
 
 -----------------------------------------------------------------------------
 -- kernel parameters
@@ -19,7 +20,7 @@ type Parameters = [(Name, Dim, Type)]
 --   Removes duplicates by name only.
 grabKernelParams :: Program a -> Parameters
 grabKernelParams = rmDup . grabKernelParams' 
-  where rmDup = nubBy $ \(n1,_,_) (n2,_,_) -> n1 == n2
+  where rmDup      = nubBy $ \(n1,_,_) (n2,_,_) -> n1 == n2
 
 grabKernelParams' :: Program a -> Parameters
 grabKernelParams' (Assign name es e) = let lhs = (name,es,typeNest TInt es) -- TODO: hard-coded to Int.
@@ -35,17 +36,17 @@ grabKernelParams' (Alloc t dim p)   = error "alloc"
 grabKernelParams' (Print t e)       = error "print"
 grabKernelParams' _                 = []
 
--- | Extracts names, types ('bit iffy atm) and type of all Indexing operations.
+-- | Extracts names, types ('bit iffy atm) and type of all array Indexing operations.
 exprAsParam :: Expr -> Parameters
-exprAsParam (Index a is) =  [(a,is, typeNest TInt is)]
-exprAsParam (a :+: b)    =  exprAsParam a ++ exprAsParam b
-exprAsParam (a :-: b)    =  exprAsParam a ++ exprAsParam b
-exprAsParam (a :/: b)    =  exprAsParam a ++ exprAsParam b
-exprAsParam (a :%: b)    =  exprAsParam a ++ exprAsParam b
-exprAsParam (a :*: b)    =  exprAsParam a ++ exprAsParam b
-exprAsParam (a :<=: b)   =  exprAsParam a ++ exprAsParam b
-exprAsParam (a :==: b)   =  exprAsParam a ++ exprAsParam b
-exprAsParam _            =  []
+exprAsParam (Index a is) = if length is > 0 then [(a,is, typeNest TInt is)] else []
+exprAsParam (a :+: b)    = exprAsParam a ++ exprAsParam b
+exprAsParam (a :-: b)    = exprAsParam a ++ exprAsParam b
+exprAsParam (a :/: b)    = exprAsParam a ++ exprAsParam b
+exprAsParam (a :%: b)    = exprAsParam a ++ exprAsParam b
+exprAsParam (a :*: b)    = exprAsParam a ++ exprAsParam b
+exprAsParam (a :<=: b)   = exprAsParam a ++ exprAsParam b
+exprAsParam (a :==: b)   = exprAsParam a ++ exprAsParam b
+exprAsParam _            = []
 
 -----------------------------------------------------------------------------
 
