4f36781d41c6b3e19252b1217cc4b052751b4746
ivar <ivar@ghostly.(none)>
2013-04-05 13:43:24 +0200

    Added monoid instance for Proc.

--- Procedure.hs
+++ Procedure.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE DeriveDataTypeable #-}
-module Procedure where
 
-import Data.Typeable
+module Procedure where
 
 import Expr
 import Program
 import Types
 import GenOCL
+
+import Data.Typeable
+import Data.Monoid
 
 data Proc a = Proc 
             { procName :: String
@@ -16,4 +18,11 @@ data Proc a = Proc
             }
   deriving (Typeable, Show)
 
+instance Monoid (Proc a) where
+  mempty      = Proc mempty mempty mempty mempty
+  mappend a b = Proc { procName = procName b
+                     , procBody = mappend (procBody a) (procBody b) 
+                     , inParams = mappend (inParams a) (inParams b)
+                     , outParam = outParam b
+                     }
 
