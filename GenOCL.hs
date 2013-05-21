8e083238f33efbd2f2e49fea2926e675c1a1c405
ivar <ivar@8560w.(none)>
2013-05-21 11:55:41 +0200

    Added String.h header

--- GenOCL.hs
+++ GenOCL.hs
@@ -256,7 +256,8 @@ setupHeadings = do tell $ mempty {pre = ["#include <stdio.h>"]}
                                         ,"#include <CL/cl.h>"
                                         ,"#endif"]}
                    tell $ mempty {pre = ["#include <math.h>"]} -- note: remember to link math. -lm
-                   tell $ mempty {pre = ["#include <time.h>"]} -- note: remember to link math. -lm
+                   tell $ mempty {pre = ["#include <time.h>"]}
+                   tell $ mempty {pre = ["#include <string.h>"]} 
                    tell $ mempty {pre = ["#include \"feldspar_c99.h\""]}
 
                    tell $ mempty {pre = ["#define MAX_SOURCE_SIZE (0x100000)\n\n"]}
